688c5be5c7e9302cf6541fd36f3510a2a427e242
hai0z <hainguyen56211@gmail.com>
2023-06-16 17:39:16 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
 <template>
-    <h1>Todo App</h1>
-    <input type="text" placeholder="enter task name" v-model="taskName" class="text-black">
-   
-   <div class="flex flex-col justify-center w-fit">
-    <div v-for="(task,index) in todoList" :key="index" class="bg-indigo-500 my-2 p-2 rounded-md cursor-pointer">
-        {{task.taskName }}
+    <div class="flex h-screen w-full flex-col bg-slate-600  items-center">
+        <h1>Todo App</h1>
+        <input type="text" placeholder="enter task name" v-model="taskName" class="text-black max-w-sm p-2 rounded-sm">
+        <div class="flex flex-col justify-center w-fit">
+            <div v-for="(task, index) in todoList" :key="index" class="bg-indigo-500 my-2 p-2 rounded-md cursor-pointer">
+                <p :class="{ 'line-through': task.done }" @click="toggleTask(task.id)"> {{ task.taskName }}</p>
+            </div>
+            <button @click="addTodo(taskName)">add</button>
+        </div>
+
     </div>
-    <button class="" @click="addTodo(taskName)">add</button>
-   </div>
-   
 </template>
 <script lang="ts">
-  export default{
-    name:'App',
-    data(){
-         return{
-            todoList:[{
-                id:1,
-                taskName:'mua do choi'
-            }] as Array<{id:number,taskName:string}>,
-            taskName:''
-         }
-    },
-   methods:{
-    addTodo(taskName: string){
-        this.todoList.push({id:Math.random()*1000, taskName:taskName})
-        this.taskName=""
+import { ref, computed } from 'vue';
+type todoType = {
+    taskName: string,
+    id: number,
+    done: boolean
+}
+export default {
+    name: 'App',
+    setup() {
+        const todoList = ref<todoType[]>([])
+        const taskName = ref<string>('')
+        const addTodo = (taskName1: string) => {
+            todoList.value.push({
+                id: Math.random() * 1000,
+                taskName: taskName1,
+                done: true
+            })
+            taskName.value = ''
+        }
+        const toggleTask = computed(() => (id: number) => {
+            todoList.value = todoList.value.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo);
+        });
+        return {
+            todoList,
+            taskName,
+            addTodo,
+            toggleTask
+        }
     }
-   }
-  }
+}
 </script>
-<style>
-</style>
+<style></style>
