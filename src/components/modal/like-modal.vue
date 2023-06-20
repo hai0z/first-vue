b15a58abc707ee191e4654fe6923d19dede21a71
hai0z <hainguyen56211@gmail.com>
2023-06-20 18:26:55 +0700

    xong ngay hom nay

--- src/components/modal/like-modal.vue
+++ src/components/modal/like-modal.vue
@@ -4,12 +4,14 @@
             <span class="font-semibold absolute right-4 top-2 cursor-pointer" @click="() => {
                 modalStore.likeModalOpen = false
                 modalStore.likeArr = undefined
+                data = []
             }">X</span>
             <span class="text-center block p-2">Likes</span>
             <div class="border-b border-zinc-300"></div>
             <div>
-                <div v-for="user in data" :key="user.uid">
-                    <span>{{ user.displayName }}</span>
+                <div v-for="user in data" :key="user.uid" class="flex ga-4 p-4">
+                    <img :src="user.photoURL" alt="" class="h-8 w-8 rounded-full">
+                    <span class="ml-2 font-semibold">{{ user.displayName }}</span>
                 </div>
             </div>
         </div>
@@ -24,13 +26,20 @@ import { ref, watch } from 'vue';
 const modalStore = useModalStore()
 const data = ref<Partial<UserInfo>[]>([])
 const getData = async (value: string[]) => {
-    const q = query(collection(db, 'users'), where('uid', 'in', value))
-    const respone = await getDocs(q);
-    data.value = respone.docs.map(doc => ({
-        uid: doc.data().uid,
-        displayName: doc.data().displayName,
-        photoURL: doc.data().photoURL,
-    }))
+    if (value.length > 0) {
+        const q = query(collection(db, 'users'), where('uid', 'in', value))
+        try {
+            const respone = await getDocs(q);
+            data.value = respone.docs.map(doc => ({
+                uid: doc.data().uid,
+                displayName: doc.data().displayName,
+                photoURL: doc.data().photoURL,
+            }))
+        } catch (err) {
+            data.value = []
+            throw err
+        }
+    }
 }
 watch(() => modalStore.likeArr, (newVal) => {
     if (newVal) {
